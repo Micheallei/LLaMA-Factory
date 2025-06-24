e74988f9d631ab79850065eb7591c0e65225eb92
t-yuxuanlei <t-yuxuanlei@microsoft.com>
2025-06-24 13:44:33 +0000

    add train pipeline

---
+++ train.sh
@@ -0,0 +1,6 @@
+export TOKENIZERS_PARALLELISM=false;
+export WANDB_RUN_ID=$(date +"%Y%m%d_%H%M%S")  # 基于当前日期时间
+
+cd /home/aiscuser/LLaMA-Factory
+
+FORCE_TORCHRUN=1 llamafactory-cli train examples/train_full/qwen2_5_full_sft.yaml > /home/aiscuser/LLaMA-Factory/output/train_qwen2.5-7b_full_sft.log 2>&1

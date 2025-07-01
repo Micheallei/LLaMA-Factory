62d8c03df5b0651d8685591b5d4ae27fd85d7e7e
yinita <1656211644@qq.com>
2025-07-01 06:02:28 +0000

    cpdc challenge finish

--- train.sh
+++ train.sh
@@ -3,4 +3,6 @@ export WANDB_RUN_ID=$(date +"%Y%m%d_%H%M%S")  # 基于当前日期时间
 
 cd /home/aiscuser/LLaMA-Factory
 
-FORCE_TORCHRUN=1 llamafactory-cli train examples/train_full/qwen2_5_full_sft.yaml > /home/aiscuser/LLaMA-Factory/output/train_qwen2.5-7b_full_sft.log 2>&1
+FORCE_TORCHRUN=1 llamafactory-cli train examples/train_full/qwen3_full_sft.yaml > /home/aiscuser/LLaMA-Factory/output/train_qwen3-14b_full_sft_from_ai_npc_v3_0626.log 2>&1
+
+python /home/aiscuser/temp2.py
